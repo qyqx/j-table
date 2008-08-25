b62a77de3e8aed9134f00b7aefb02460036680fb
chris.f.jay <chris.f.jay@1f96bf71-fa50-0410-82c2-5db472f112cb>
2008-08-25 21:00:44 +0000

    issue 19 another attempt, this time everything except UI and sort

--- testsuite.js
+++ testsuite.js
@@ -70,54 +70,48 @@ var tc_jt_table = new YAHOO.tool.TestCase({
     }
 });
 var tc_jt_datatype = new YAHOO.tool.TestCase({
-	name: "jt.datatype",
-	testGetTDataType: function() {
-	    assert.areEqual(jt('testTable').dataType(0), 'string', "incorrect table data types");
-	    assert.areEqual(jt('testColHeader').dataType(), 'number', 'incorrect column data types');
-	    assert.areEqual(jt('testCell1').dataType(), 'number', 'incorrect column data type');
-	}
+    name: "jt.datatype",
+    testGetTDataType: function() {
+        assert.areEqual(jt('testTable').dataType(0), 'string', "incorrect table data types");
+        assert.areEqual(jt('testColHeader').dataType(), 'number', 'incorrect column data types');
+        assert.areEqual(jt('testCell1').dataType(), 'number', 'incorrect column data type');
+    }
 });
-var tc_jTable_t = new YAHOO.tool.TestCase({
-	name: "jTable.t",
-	setUp : function () {
-	    this.dataTable = jTable.t('testTable');
-	},
-	tearDown : function () {
-	    delete this.dataTable;
-	},
-	testInputTable: function () {
-	    assert.isObject(jTable.t('testTable').rows, "jTable.t() should accept DOM table parameter");
-	},
-	testInputTableRow: function () {
-	    assert.isObject(jTable.t('firstTableRow').rows, "jTable.t() should accept DOM table row parameter");
-	},
-	testInputTableCell: function () {
-	    assert.isObject(jTable.t('firstTableCell').rows, "jTable.t() should accept DOM table cell parameter");
-	},
-	testInputString: function () {
-	    assert.isObject(jTable.t('testTable').rows, "jTable.t() should accept string parameter");
-	},	
-	testInputNotTable: function () {
-	    assert.areEqual(jTable.t('menu'), undefined, "jTable should not allow non-table parameter");
-	}
+var tc_jt_hide = new YAHOO.tool.TestCase({
+    name: "jt.hide",
+    testHeaderHide: function () {
+        assert.areEqual(jt('testColHeader').getHide(), false, "jt().getHide() didn't work");
+        jt('testColHeader').setHide(true);
+        assert.areEqual(jt('testColHeader').getHide(), true, "jt().setHide() didn't work");
+        jt('testColHeader').setHide(false);
+    },
+    testTableHide: function () {
+        assert.areEqual(jt('testTable').getHide(1), false, "jt().getHide() didn't work");
+        jt('testTable').setHide(true, 1);
+        assert.areEqual(jt('testTable').getHide(1), true, "jt().setHide() didn't work");
+        jt('testTable').setHide(false, 1);
+    }
 });
-var tc_jTable_h = new YAHOO.tool.TestCase({
-	name: "jTable.h",
-	setUp : function () {
-	    this.data = jTable.h('firstTableCell');
-	},
-	tearDown : function () {
-	    delete this.data;
-	},
-	testInputTableCell: function () {
-	    assert.areEqual(this.data.tagName.toLowerCase(), "th", "jTable.t() should accept DOM cell parameter");
-	},
-	testInputString: function () {
-	    assert.areEqual(jTable.h('firstTableCell').tagName.toLowerCase(), "th", "jTable.t() should accept string parameter");
-	},	
-	testInputNotTableCell: function () {
-	    assert.areEqual(jTable.h('menu'), undefined, "jTable should not allow non-table parameter");
-	}
+var tc_jt_filter = new YAHOO.tool.TestCase({
+    name: "jt.filter",
+    testHeaderFilter: function () {
+        assert.areEqual(jt('testColHeader').getFilter(), undefined, "jt().getFilter() didn't return an empty object");
+        var header = jt('testColHeader').setFilter(/^50|60$/);
+        var rows = jt('testTable').tBodies[0].rows;
+        for (var i = 0; i < rows.length; i++) {
+            assert.areEqual(rows[i].className.search("filtered") >= 0, header.data(i) === "100", "setFilter didn't filter properly");
+        }
+        jt('testColHeader').setFilter();
+    },
+    testTableFilter: function () {
+        assert.areEqual(jt('testTable').getFilter(1), undefined, "jt().getFilter() didn't return an empty object");
+        var header = jt('testTable').setFilter(/^50|60$/, 1).headerCell(1);
+        var rows = jt('testTable').tBodies[0].rows;
+        for (var i = 0; i < rows.length; i++) {
+            assert.areEqual(header.cell(i).parentNode.className.search("filtered") >= 0, header.data(i) === "100", "setFilter didn't filter properly");
+        }
+        jt('testTable').setFilter(undefined, 1);
+    }
 });
 var tc_jTable_t_sort = new YAHOO.tool.TestCase({
 	name: "jTable.t.sort",
@@ -183,87 +177,6 @@ var tc_jTable_h_sort = new YAHOO.tool.TestCase({
 	    assert.areEqual(this.data.data(1), '60', 'setSort did not sort correctly');
 	}
 });
-var tc_jTable_datatype = new YAHOO.tool.TestCase({
-	name: "jTable.datatype",
-	testGetTDataType: function() {
-	    assert.areEqual(jTable.t('testTable').dataType().join(), 'string,number,number,number,number', "incorrect table data types");
-	    assert.areEqual(jTable.h('testColHeader').dataType(), 'number', 'incorrect column data types');
-	}
-});
-var tc_jTable_h_hide = new YAHOO.tool.TestCase({
-	name: "jTable.h.hide",
-	testGetHide: function () {
-	    assert.areEqual(jTable.h('testColHeader').getHide(), false, "jTable.h().getHide() didn't work");
-	},
-	testSetHide: function() {
-	    jTable.h('testColHeader').setHide(true);
-	    assert.areEqual(jTable.h('testColHeader').getHide(), true, "setHide didn't work");
-	    jTable.h('testColHeader').setHide(false);
-	}
-});
-var tc_jTable_t_hide = new YAHOO.tool.TestCase({
-	name: "jTable.t.hide",
-	testGetHide: function () {
-	    assert.areEqual(jTable.t('testTable').getHide().length, 0, "jTable.t().getHide() should return an empty array");
-	},
-	testSetHide: function() {
-	    jTable.t('testTable').setHide([1,3]);
-	    assert.areEqual(jTable.t('testTable').getHide().length, 2, "setHide didn't hide every column");
-	    assert.areEqual(jTable.t('testTable').getHide()[0], 1, "setHide didn't hide the first column");
-	    assert.areEqual(jTable.t('testTable').getHide()[1], 3, "setHide didn't hide the second column");
-	    jTable.t('testTable').setHide([]);
-	}
-});
-var tc_jTable_h_filter = new YAHOO.tool.TestCase({
-	name: "jTable.h.filter",
-	testGetFilter: function () {
-	    assert.areEqual(jTable.h('testColHeader').getFilter(), undefined, "jTable.h().getFilter() didn't return an empty object");
-	},
-	testSetFilter: function() {
-	    var header = jTable.h('testColHeader').setFilter(/^50|60$/);
-	    var rows = jTable.t('testColHeader').tBodies[0].rows;
-	    for (var i = 0; i < rows.length; i++) {
-	        assert.areEqual(rows[i].className.search("filtered") >= 0, header.data(i) === "100", "setFilter didn't filter properly");
-	    }
-	}
-});
-var tc_jTable_t_filter = new YAHOO.tool.TestCase({
-	name: "jTable.t.filter",
-	tearDown : function () {
-	    jTable.t('testTable').setFilter([]);
-	},
-	testGetFilter: function () {
-	    jTable.t('testTable').setFilter([]);
-	    assert.areEqual(jTable.t('testTable').getFilter().length, 0, "jTable.t().getFilter() should return an empty array");
-	},
-	testSetFilter: function() {
-	    jTable.t('testTable').setFilter([{cellIndex: 1, filter: /^50|60$/}]);
-	    assert.areEqual(jTable.t('testTable').getFilter().length, 1, "setFilter didn't hide every column");
-	    var rows = jTable.t('testColHeader').tBodies[0].rows;
-	    var textContent;
-	    for (var i=0; i<rows.length; i++) {
-	        assert.areEqual(rows[i].className.search("filtered") >= 0, jTable.t('testTable').data(i, 0) == "USA", "setFilter didn't filter properly");
-	    }
-	}
-});
-var tc_jTable_c = new YAHOO.tool.TestCase({
-	name: "jTable.c",
-	setUp : function () {
-	    this.data = jTable.c('testColHeader');
-	},
-	tearDown : function () {
-	    delete this.data;
-	},
-	testInputTableCell: function () {
-	    assert.areEqual(jTable.c(document.getElementById('testColHeader')).tagName.toLowerCase(), "th", "jTable.t() should accept DOM cell parameter");
-	},
-	testInputString: function () {
-	    assert.areEqual(jTable.c('testColHeader').tagName.toLowerCase(), "th", "jTable.t() should accept string parameter");
-	},	
-	testInputNotTableCell: function () {
-	    assert.areEqual(jTable.c('menu'), undefined, "jTable should not allow non-table parameter");
-	}
-});
 var tc_jt_editmode = new YAHOO.tool.TestCase({
 	name: "jt.editMode",
 	setUp : function () {
@@ -280,19 +193,6 @@ var tc_jt_editmode = new YAHOO.tool.TestCase({
 	    assert.areEqual(this.data.getElementsByTagName("div").length, 1, "jt.setEditMode() did not make a cell editable");
 	    assert.areEqual(this.data.getEditMode(), true, "jt.setEditMode did not update jt.getEditMode");
 	    this.data.setEditMode(false);
-	}
-});
-var tc_jTable_h_addremovecolumn = new YAHOO.tool.TestCase({
-	name: "jTable.h.addRemoveColumn",
-	testAddColumn: function () {
-	    jTable.h('testColHeader').addColumn(false);
-	    jTable.h('testColHeader').addColumn(true);
-	    assert.areEqual(jTable.h('testColHeader').parentNode.cells.length, 7, "jTable.h.addColumn didn't increment the header count");
-	},
-	testRemoveColumn: function() {
-	    jTable.h(jTable.h('testColHeader').nextSibling).deleteColumn();
-	    jTable.h(jTable.h('testColHeader').previousSibling).deleteColumn();
-	    assert.areEqual(jTable.h('testColHeader').parentNode.cells.length, 5, "jTable.h.addColumn didn't decrement the header count");
 	}
 });
 var tc_jt_addremovecolumn = new YAHOO.tool.TestCase({
@@ -328,18 +228,11 @@ YAHOO.tool.TestRunner.add(tc_jt_headercell);
 YAHOO.tool.TestRunner.add(tc_jt_headerdata);
 YAHOO.tool.TestRunner.add(tc_jt_table);
 YAHOO.tool.TestRunner.add(tc_jt_datatype);
-YAHOO.tool.TestRunner.add(tc_jTable_t);
-YAHOO.tool.TestRunner.add(tc_jTable_h);
+YAHOO.tool.TestRunner.add(tc_jt_hide);
+YAHOO.tool.TestRunner.add(tc_jt_filter);
 YAHOO.tool.TestRunner.add(tc_jTable_t_sort);
 YAHOO.tool.TestRunner.add(tc_jTable_h_sort);
-YAHOO.tool.TestRunner.add(tc_jTable_datatype);
-YAHOO.tool.TestRunner.add(tc_jTable_h_hide);
-YAHOO.tool.TestRunner.add(tc_jTable_t_hide);
-YAHOO.tool.TestRunner.add(tc_jTable_h_filter);
-YAHOO.tool.TestRunner.add(tc_jTable_t_filter);
-YAHOO.tool.TestRunner.add(tc_jTable_c);
 YAHOO.tool.TestRunner.add(tc_jt_editmode);
-//YAHOO.tool.TestRunner.add(tc_jTable_h_addremovecolumn);
 YAHOO.tool.TestRunner.add(tc_jt_addremovecolumn);
 var oLogger = new YAHOO.tool.TestLogger(); 
 YAHOO.tool.TestRunner.run();
