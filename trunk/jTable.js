bca6ce55b831606482192bea9174d2cf4c31ccff
chris.f.jay <chris.f.jay@1f96bf71-fa50-0410-82c2-5db472f112cb>
2008-10-10 22:03:40 +0000

    fix issues 47 and 48, convert so that jQuery now runs sort / hide / filter directly

--- trunk/jTable.js
+++ trunk/jTable.js
@@ -5,32 +5,27 @@ var colOptions = {
 	//first, get the unique entries in the list into an array. then sort array.
 	this.colHead = colHead;
 	var filter = colHead.columnFilter()[0];
-	var uniqueColValues = [];
-        var cellIndex = colHead.attr("cellIndex");
-        var cloneHeader = colHead.table().clone().tableSort([{cellIndex: cellIndex, dir: 'up'}]).column(cellIndex);
-        var i = 0;
-        while (cloneHeader.cell(i)) {
-            if (i===0 || cloneHeader.cell(i).text() !== cloneHeader.cell(i - 1).text()) {
-                uniqueColValues.push(cloneHeader.cell(i).text());    
-            }
-            i++;
-        }
-        //position box and create content
+        var i = 0, val = "";
+        var colSelector = "*:nth-child(" + (this.colHead.attr("cellIndex") + 1) + ")";
+	//set up initial values
         this.div.find('input').removeAttr("checked");
-        this.div.find("input[name='sort']:first").attr("checked", colHead.tableSort() == 'up');
-	this.div.find("input[name='sort']:last").attr("checked", colHead.tableSort() == 'down');
+        this.div.find("input[name='sort']:first").attr("checked", colHead.attr("data-sort") == 'asc');
+	this.div.find("input[name='sort']:last").attr("checked", colHead.attr("data-sort") == 'desc');
 	this.div.find('h3').html(colHead.html());
 	this.div.find('#colOptionsFilter span').remove();
-	var str = this.div.find('#colOptionsFilter').html();
-	for (var i=0; i<uniqueColValues.length; i++) {
-	    str += "<span class='divOption'><input type='checkbox' name='chkname" + i + "' ";
-	    str += "id='chkid" + i + "' value='" + uniqueColValues[i] + "' ";
-	    if (!filter || (filter instanceof RegExp && filter.test(uniqueColValues[i]))) {
-	 	str += "checked";
+	var filterHTML = this.div.find('#colOptionsFilter').html();
+	//populate set of filter values
+	colHead.parents("table").clone().find("tbody tr").tsort(colSelector).find(colSelector).each(function (i) {
+	    val = $(this).text()
+	    filterHTML += "<span class='divOption'><input type='checkbox' name='chkname" + i + "' ";
+	    filterHTML += "id='chkid" + i + "' value='" + val + "' ";
+	    if (!filter || (filter instanceof RegExp && filter.test(val))) {
+	     	filterHTML += "checked";
 	    }
-            str += "></input><label for='chkid" + i + "'>" + uniqueColValues[i] + "</label></span>";
-	}
-	this.div.find('#colOptionsFilter').html(str);
+            filterHTML += "></input><label for='chkid" + i + "'>" + val + "</label></span>";
+	});
+	this.div.find('#colOptionsFilter').html(filterHTML);
+        //position box;
 	this.div.css({
 	    left: colHead.offset().left + colHead.outerWidth() + "px", 
 	    top: colHead.offset().top + "px", 
@@ -47,35 +42,61 @@ var colOptions = {
     submit: function() {
 	//filter
 	var filterInputs = this.div.find("#colOptionsFilter input");
-	var filterArray = [];
-	for (var i=0; i<filterInputs.length; i++) {
-	    if (filterInputs.eq(i).attr("checked")) {
-		filterArray.push(filterInputs.eq(i).attr("value"));
-	    }
-	}
+	var filterArray = filterInputs.filter("[checked]").map(function() {
+	    return $(this).val();
+	}).get();
+	var regExp;
+	var table = this.colHead.parents("table");
+        var rows = table.find("tbody tr");
+        var headers = table.find("thead th");
+        var colSelector = "*:nth-child(" + (this.colHead.attr("cellIndex") + 1) + ")";
+        var filterIn = rows;
+        //fix filter attribute 
 	if (filterArray.length == filterInputs.length) {
-	    this.colHead.columnFilter(false);
+	    this.colHead.removeAttr("data-filter");
 	} else {
-            this.colHead.columnFilter(new RegExp("^" + filterArray.join("|") + "$"));
+            regExp = new RegExp("^" + filterArray.join("|") + "$")
+            this.colHead.attr("data-filter", regExp);
         }
-	//sort
-	if (this.div.find('#sort_ascending').attr("checked"))
-	    this.colHead.tableSort("up");
-	if (this.div.find('#sort_descending').attr("checked"))
-	    this.colHead.tableSort("down");
+        //apply filters from each column to end up with remaining visible rows
+        headers.each(function (i) {
+            if (this.getAttribute("data-filter")) {
+                regExp = /^\/(.+)\/$/.exec(this.getAttribute("data-filter"));
+                if (regExp) {
+                    filterIn = filterIn.filter(function (index) {
+                        return (new RegExp(regExp[1])).test(this.cells[i].textContent);
+                    });
+                }
+            }
+        });
+        rows.css("display", "none");
+        filterIn.css("display", "");
+        //sort ascending
+	var sortDir = this.div.find('#colOptionsSort input[checked]').val();
+	if (sortDir) {
+	    this.colHead.attr("data-sort", sortDir);
+	    table.attr("data-sortOrder", this.colHead.attr("cellIndex"));
+	    rows.tsort(colSelector, {order: sortDir});
+	}
 	//hide
 	if (this.div.find('#chkHide').attr("checked")) {
-	    this.colHead.columnHide(true);
+	    table.find("tr " + colSelector).css("display", "none");
 	}
 	//add Col
 	if (this.div.find('#addColLeft').attr("checked")) {
-	    this.colHead.columnAdd(false);
+	    //this.colHead.columnAdd(false);
+	    table.find("thead tr " + colSelector).before("<th></th>");
+	    rows.find(colSelector).before("<td></td>");
 	}
 	if (this.div.find('#addColRight').attr("checked")) {
-	    this.colHead.columnAdd(true);
+	    //this.colHead.columnAdd(true);
+	    table.find("thead tr " + colSelector).after("<th></th>");
+	    rows.find(colSelector).after("<td></td>");	   
 	}
 	if (this.div.find('#chkDelete').attr("checked")) {
-	    this.colHead.columnRemove();
+	    //this.colHead.columnRemove();
+	    table.find("thead tr " + colSelector).remove();
+	    rows.find(colSelector).remove();	  	    
 	}
 	this.div.find('#chkHide').attr("checked", "false");
 	this.close();
@@ -86,11 +107,11 @@ var tableTransform = {
     table: undefined,
     div: $('#tableTransform'),
     open: function(elem) {
- 	this.table = elem.table();
-	// unhide
+ 	this.table = elem.parents("table");
+ 	// unhide
 	var strUnhide = "";
 	this.table.find("thead th").each(function (i) {
-	    if (jTable.columnHide(this)) {
+	    if (this.style.display === "none") {
 	        strUnhide += '<span class="divOption"><input type="checkbox" name="chkUnhide" id="chkHide' + i + '" value="' + i + '">';
 		strUnhide += '<label for="chkHide' + i + '">' + $(this).text() + '</label></span>';
 	    }
@@ -111,54 +132,20 @@ var tableTransform = {
     },
     submit: function() {
 	//unhide
-	var table = this.table;
+	var tbl = this.table;
 	this.div.find("input[name='chkUnhide'][checked]").each(function (i) {
-	    table.columnHide(this.value, false);
+	    tbl.find("tr *:nth-child(" + (parseInt(this.value, 10) + 1) + ")").css("display", "table-cell");
 	});
 	this.close();
     }
 };
 
-function getRange() {
-	var userSelection;
-	if (window.getSelection)
-		userSelection = window.getSelection();
-	else if (document.selection)
-		userSelection = document.selection.createRange();
-
-	if (userSelection.getRangeAt)
-		return userSelection.getRangeAt(0);
-	else { // Safari!
-		var range = document.createRange();
-		range.setStart(userSelection.anchorNode,userSelection.anchorOffset);
-		range.setEnd(userSelection.focusNode,userSelection.focusOffset);
-		return range;
-	}
-}
 var menu = {
-    clickTab: function(tab) {
-        var tabHeaders = document.getElementById('tabHeaders').getElementsByTagName('li');
-	for (var i=0; i<tabHeaders.length; i++) {
-	    if (tabHeaders[i].innerHTML.indexOf(tab) > -1)
-		tabHeaders[i].className = 'selected';
-	    else
-		tabHeaders[i].className = '';
-	    }
-	    var tabContents = document.getElementById('tabContents').getElementsByTagName('ul');
-	    for (var i=0; i<tabContents.length; i++) {
-	    if (tabContents[i].id.indexOf(tab) > -1)
-	 	tabContents[i].className = 'selected';
-	    else
-		tabContents[i].className = '';
-	}
-    },
-    refresh: function(tbl) {
-    // display as
-	var rad = document.getElementById('display_as');
-	for (var i=0; i<rad.length; i++) {
-	    if (rad[i].value == tbl.getAttribute("viewAs"))
-		rad[i].checked = true;
-	}
+    clickTab: function(e) {
+        $('#tabHeaders li').removeClass("selected");
+        $(e.target ? e.target : e.srcElement).parent().addClass("selected");
+        $('#tabContents ul').removeClass("selected");
+        $('#tabContents ul[id=tab' + $('#tabHeaders li.selected').text() + ']').addClass("selected");
     },
     style: function(cmd, str) {
 	if (document.queryCommandEnabled) {
@@ -169,37 +156,48 @@ var menu = {
 	}
     }
 };
+
 function mDown(e) {
     var e = e ? e : window.event;
     var elem = $(e.target ? e.target : e.srcElement);
     if (!elem.table()) {
         return false;
     }
-    var tableOffsetLeft = elem.table().offset().left;
-    //remove all editCells;
-    $('#editCell').cellEditMode(false);
+    var table = elem.parents("table");
+    var tableOffsetLeft = table.offset().left;
+    var removeEditCell = function() {
+        table.find("td div[contenteditable], th div[contenteditable]").each(function (i) {
+            $(this).contents().appendTo($(this).parent());
+            $(this).remove();
+        });
+    }
     //is it the sort/filter option in the header cell?
-    if (elem.is("td, th")) {
+    if ($('thead th, thead td').filter(function() { return this === elem.get()[0]; }).length > 0) {
         if (e.clientX > (elem.offset().left + elem.outerWidth() - 10)) {
             colOptions.open(elem);
+            removeEditCell();
 	    return;
 	}
 	if (e.clientX > (elem.offset().left + elem.outerWidth() - 20)) {
-   	    if (e.ctrlKey) {
-   	        if (!elem.table().tableSort()) {
-   	            elem.table().tableSort(elem.table().tableSort()[0].concat([{cellIndex: elem.cellIndex, 'dir': 'up'}]));
-   	        }
-   	    } else {
-   	        elem.tableSort(elem.tableSort() == 'up' ? 'down' : 'up');
-   	    }
+   	    var sortDir = elem.attr("data-sort") == 'asc' ? 'desc' : 'asc';
+	    elem.attr("data-sort", sortDir);
+	    table.attr("data-sortOrder", elem.attr("cellIndex"));
+	    table.find("tbody tr").tsort("*:nth-child(" + (elem.attr("cellIndex") + 1) + ")", {order: sortDir});
+	    removeEditCell();
    	    return;
    	}
-   	elem.cellEditMode(true);
-	elem.get()[0].getElementsByTagName("div")[0].focus();
+    }
+    if ($('th, td').filter(function() { return this === elem.get()[0]; }).length > 0) {   	
+        //enable editCell if it isn't already
+   	if (elem.find("div[contenteditable]").length === 0) {
+   	    removeEditCell();
+   	    elem.contents().wrap("<div contenteditable=true></div>");
+   	}
+	elem.find("div[contenteditable]").focus();
 	return;
     }
     //is it the table transform dialog box?
-    if ((e.target ? e.target : e.srcElement).tagName.toLowerCase() === 'caption' && e.clientX > (elem.offset().left + elem.outerWidth()  - 30)) {
+    if (elem.filter("caption").length > 0 && e.clientX > (elem.offset().left + elem.outerWidth()  - 30)) {
 	tableTransform.open(elem);
 	return;
     }
